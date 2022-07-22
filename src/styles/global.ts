adfc9705a2ec50b76f982c8f7a048d3fe2f77e41
Ivan Nalon <ivannalon@gmail.com>
2022-07-22 12:22:46 -0300

    global font's

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,4 +11,10 @@ export const GlobalStyle = createGlobalStyle`
     background: #333;
     color: #ffffff;
   }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
 `;
