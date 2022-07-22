d5fa99732d3734f34f0585717d5ccfdd48dc944d
Ivan Nalon <ivannalon@gmail.com>
2022-07-22 12:07:11 -0300

    create theme exemple

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+
 export function App() {
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
       Hello word!
-    </div>
+    </ThemeProvider>
   )
 }
