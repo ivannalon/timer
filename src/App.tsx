9cdf65bc3e19bb23c5417800265bb404758d1934
Ivan Nalon <ivannalon@gmail.com>
2022-07-22 12:39:11 -0300

    refactor eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/themes/default"
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       Hello word!
-
-      <GlobalStyle/>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
